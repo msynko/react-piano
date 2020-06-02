ab00e12ffbf5274e3ec2b07fcf19aa6a696e504a
m_sinko9730 <maryna@thinkingbox.com>
2020-06-02 19:13:49 -0400

    Updates

---
+++ precache-manifest.c3020d27b00be76dac8bae7fc38a6a59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea1584c2a826668861a8e4986d31ad1",
+    "url": "/react-piano/index.html"
+  },
+  {
+    "revision": "6b494787d8bceb6b3ccb",
+    "url": "/react-piano/static/css/main.affd6eb5.chunk.css"
+  },
+  {
+    "revision": "34727a1ce2b89fd28d1c",
+    "url": "/react-piano/static/js/2.085fedeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-piano/static/js/2.085fedeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b494787d8bceb6b3ccb",
+    "url": "/react-piano/static/js/main.de0936aa.chunk.js"
+  },
+  {
+    "revision": "3d3e86053d602b415934",
+    "url": "/react-piano/static/js/runtime-main.ef23768d.js"
+  },
+  {
+    "revision": "be7e71fe84a79140fa5dfdc75f982898",
+    "url": "/react-piano/static/media/notes.be7e71fe.jpg"
+  }
+]);
